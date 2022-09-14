8e5130082a518161070f60632ee9cc0870d2a6cc
lissa2077 <71573064+lissa2077@users.noreply.github.com>
2022-09-14 23:59:02 +0800

    Update index.js

--- Canvas/index.js
+++ Canvas/index.js
@@ -23,6 +23,7 @@ class Sprite {
     update(){
         this.draw();
         
+        this.position.x += this.velocity.x;
         this.position.y += this.velocity.y;
 
         if(this.position.y+this.height >= canvas.height){
@@ -58,16 +59,56 @@ const enemy = new Sprite({
 player.draw();
 enemy.draw();
 
+const keys = {
+    a:{
+        pressed: false
+    },
+    d:{
+        pressed: false
+    }
+}
+let lastKey;
+
 function animate(){
     window.requestAnimationFrame(animate); //this function
     c.fillStyle = 'black';
     c.fillRect(0,0,canvas.width,canvas.height);
     player.update();
     enemy.update();
+
+    player.velocity.x = 0;
+    if(keys.a.pressed && lastKey === 'a'){
+        player.velocity.x = -1;
+    }else if(keys.d.pressed && lastKey === 'd'){
+        player.velocity.x = 1;
+    }
 }
 
 animate();
 
 window.addEventListener('keydown',(event)=>{
     console.log(event.key);
+    switch(event.key){
+        case 'd':
+            keys.d.pressed = true;
+            lastKey = 'd';
+            break;
+        case 'a':
+            keys.a.pressed = true;
+            lastKey = 'a';
+            break;
+    }
+})
+
+window.addEventListener('keyup',(event)=>{
+    console.log(event.key);
+    switch(event.key){
+        case 'd':
+            keys.d.pressed = false;
+            break;
+        case 'a':
+            keys.a.pressed = false;
+            break;
+        
+    }
 })
